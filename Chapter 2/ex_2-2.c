806b07a6cca15d4d6101cd20e6f25445d34ff12d
unnicolo <nico.dreessen93@gmail.com>
2016-05-04 19:21:14 +0200

    first commit

---
+++ Chapter 2/ex_2-2.c
@@ -0,0 +1,34 @@
+/*  Exercise 2.2 - The C Programming Language
+**  Write a loop equivalent to the following loop without using && or ||
+**  for (int i = 0; i<lim-1 && (c=getchar()) != '\n' && c != EOF; ++i)
+**      s[i] = c;
+*/
+
+#include <stdio.h>
+
+#define MAXSIZE 1000    // max size of the line
+
+void loop_test(char line[], int size);
+
+int main(void)
+{
+    int i, c;
+    char s[MAXSIZE];
+    
+    for (i = 0; i<MAXSIZE-1; ++i) {
+        if ((c=getchar()) != '\n') {
+            if (c != EOF)
+                s[i] = c;
+        } else
+            break;
+    }
+    loop_test(s, i);
+}
+
+void loop_test(char line[], int length)
+{
+    for (int i = 0; i<length; ++i) {
+        printf("%c", line[i]);
+    }
+    printf("\n");
+}
---
+++ Chapter 2/ex_2-3.c
@@ -0,0 +1,58 @@
+/*  Exercise 2.3 - The C Programming Language
+**  Write the function htoi(s), which converts a string of
+**  hexadecimal digits (including an optional 0x or 0X) into its
+**  equivalent integer value.
+**  !strings are assumed to be given in little endian!
+*/
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+
+#define MAXSIZE 50  // maximum of 50 characters
+
+int htoi(char hex_string[]);
+void string_lower(char string[]);
+
+int main(void)
+{
+    char s[] = "0XFA327";           // as integer: 467887
+    int i;  // counter variable
+    int c;
+    long n;
+    
+    n = htoi(s);
+    printf("%li", n);
+    printf("\n");
+}
+
+/* htoi: converts a string of hex-digits into its integer equivalent */
+int htoi(char s[])
+{
+    int i, c;
+    long n, debug_res;
+    
+    string_lower(s);
+    if (s[0] == '0' && s[1] == 'x') {   // check if 0X or 0x is leading prefix
+        i = 2;  // begin transforming at 3rd character
+    } else
+        i = 0;
+        
+    n = 0;
+    for (int j = 0; i<MAXSIZE-1 && (c=s[i]) != '\0'; ++i, ++j) {
+        if (!isxdigit(c))
+            printf("Invalid hex character... Program termination\n");
+        if (isdigit(c))
+            n += (c - '0') * pow(16, j);
+        else if (isalpha(c))
+            n += (c - 'a' + 10) * pow(16, j);
+    }
+    return n;
+}
+
+/*  string_lower: converts a string to lower-case */
+void string_lower(char s[])
+{
+    int i, c;
+    for (i = 0; i<MAXSIZE-1 && (c=s[i]) != '\0'; ++i)
+        s[i] = tolower(c);  // convert all letters to lower case
+}
