237219b68bbd8e7a57d3ee740f69b50d04429d83
unnicolo <nico.dreessen93@gmail.com>
2016-05-15 17:41:18 +0200

    update

--- Chapter 2/ex_2-6.c
+++ Chapter 2/ex_2-6.c
@@ -24,7 +24,8 @@ unsigned getbits(unsigned x, int p, int n) {
     return (x >> (p+1-n)) & ~(~0 << n);
 }
 
-/*  setbits: returns an integer x with the n bits that begin at position p
+/*  
+**  setbits: returns an integer x with the n bits that begin at position p
 **  set to the rightmost n bits of y, leaving the other bits unchanged
 */
 unsigned setbits(unsigned x, int p, int n, unsigned y) {
