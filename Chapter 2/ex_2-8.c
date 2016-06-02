a700d76a8f7dcb3018a2c3517df13a389ebbd3e4
unnicolo <nico.dreessen93@gmail.com>
2016-06-02 21:29:41 +0200

    exercise 2-8

---
+++ Chapter 2/ex_2-8.c
@@ -0,0 +1,23 @@
+/*
+**  Exercise 2-6 - The C Programming Language
+**  Created 13.05.2016
+*/
+
+#include <stdio.h>
+
+unsigned rightrot(unsigned x, int n);   // prototype
+
+int main(void) {
+    unsigned x, res;
+    int n;
+    x = 1024;
+    n = 2;
+    res = rightrot(x, n);
+    printf("%u rotated by %i yields %u\n", x, n, res);    // should yield 256
+    printf("expected value is 256\n");
+}
+
+/*  rightrot: returns the value of x rotated to the right by n bit positions */
+unsigned rightrot(unsigned x, int n) {
+    return x >> n;
+}
